8b07132dc8a56720488ffcdcf489d47638305814
Ritvik721 <101156313+Ritvik721@users.noreply.github.com>
2022-06-01 21:06:56 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+// Create a variable canvas
+var canvas = new fabric.canvas('myCanvas')
+
+block_y=1;
+block_x=1;
+
+block_image_width = 350;
+block_image_height = 480;
+
+var block_image_object= "";
+
+// Complete the function new_image() to add new image
+function new_image(get_image)
+{
+fabric.image.fromURL(get_image, funtion(Img) {
+block_image_object = Img,
+block_image_object.scaleToWidth(b)
+
+
+
+
+	
+
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+	// Use appropriate keycode to add red image
+	if(keyPressed == ) 
+	{
+
+
+
+	}
+	// Use appropriate keycode to add green image
+	if(keyPressed == )
+	{
+	
+
+
+	}
+	// Use appropriate keycode to add yellow image
+	if(keyPressed == )
+	{
+
+		
+
+	}
+	// Use appropriate keycode to add pink image
+	if(keyPressed == )
+	{
+	
+
+
+	}
+	// Use appropriate keycode to add blue image
+	if(keyPressed == )
+	{
+
+
+
+	}
+	
+}
+
